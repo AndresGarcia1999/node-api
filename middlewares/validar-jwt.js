dcc07417e7715fd2184c53f6b9ff8be87f846d64
Andrés Felipe García Marulanda <andres-ssj@hotmail.com>
2023-01-28 15:04:55 -0500

    Fin sescción 10 - ver error token

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -40,7 +40,8 @@ const validarJWT = async(req = request, res = response, next ) => {
     } catch (error) {
         //console.log(error);
         res.status(401).json({
-            msg : 'token no valido'
+            msg : 'token no valido',
+            error
         })
     }
 
